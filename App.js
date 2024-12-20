97d552faff2c888330a3c330ae572567523055bf
Nurtilek <nurtilek.azhybekov@mail.ru>
2024-12-21 00:28:32 +0300

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import Loading from './Loading';
+ 
+export default class extends React.Component {
+  
+  getLocation = async () => {
+    try {
+     const response = await Location.requestForegroundPermissionsAsync();
+     console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location); 
+    } catch (error) {
+      Alert.alert('Не могу определить местоположение', "Очень грустно:(")
+    }   
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render () {
+    return (
+      <Loading/>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
